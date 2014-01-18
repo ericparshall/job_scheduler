c4f4e67d188632e1a8ea37240b494bb0c13629c4
Eric Parshall <ericparshall@gmail.com>
2014-01-18 15:16:09 -0700

    .deliver

--- app/controllers/time_off_requests_controller.rb
+++ app/controllers/time_off_requests_controller.rb
@@ -28,7 +28,7 @@ class TimeOffRequestsController < ApplicationController
     @time_off_request = TimeOffRequest.new(time_off_request_params.merge(manager_id: manager_id, user_id: user_id))
 
     if @time_off_request.save
-      TimeOffMailer.time_off_request(current_user, @time_off_request)
+      TimeOffMailer.time_off_request(current_user, @time_off_request).deliver
       redirect_to @time_off_request, notice: 'Time off request was successfully created.'
     else
       render action: 'new'
