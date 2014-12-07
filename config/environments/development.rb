34c3f359ca0c992937efb5a0a9c04f899cc45c25
Eric Parshall <ericparshall@gmail.com>
2014-12-06 23:38:04 -0700

    turn off compression

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -21,7 +21,6 @@ JobScheduler::Application.configure do
 
   # Do not compress assets
   config.assets.compress = false
-  config.assets.js_compressor = NoCompression.new
 
   # Expands the lines which load the assets
   config.assets.debug = true
